a6c8ac95dbd9b810857521902cfed6b35f3a7844
crississipi <126639193+crississipi@users.noreply.github.com>
2024-11-26 09:55:26 +0800

    Add files via upload

---
+++ FINALS-PROJECT_Malipico-Cris-Julius/login.php
@@ -0,0 +1,26 @@
+<?php
+include("connect.php");
+    $email = $_POST['lgmail'];
+    $pass = $_POST['lpass'];
+
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $pass = filter_var($pass, FILTER_DEFAULT);
+
+    $sql = "SELECT * FROM userinfo WHERE email = '$email' OR username = '$email';";
+    $output = $connect->query($sql);
+
+    if ($output->num_rows > 0) {
+        $user = $output->fetch_assoc();
+        if (password_verify($pass, $user['pass'])) {
+            session_start();
+            $_SESSION["userid"] = $user['UIID'];
+            $_SESSION["username"] = $user['fname'] . " " . $user['lname'];
+            $alert = "0";
+        } else {
+            $alert = "1";
+        }
+    } else {
+        $alert = "1";
+    }
+    echo $alert;
+?>
